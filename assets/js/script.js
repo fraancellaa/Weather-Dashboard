6ddc49ec1b6165060a241af65563d3d84321d31a
Francella Aguilar <francellaaguilar@Francellas-MacBook-Pro.local>
2022-04-09 14:55:10 -0400

    setting up javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var weatherDetailContainer = document.querySelector("#cityWeather");
+var searchBtn = document.querySelector("#searchBtn");
+var citySearchDiv = document.querySelector("#input-group");
+var cityFormEl = document.querySelector("#city-search-form");
+var cityInputEl = document.querySelector("#cityInput");
+var forecastContainerEl = document.querySelectorAll("#forecast-container");
+var searchHistoryBtn = document.querySelectorAll("#search-history-btn");
+var cities = [];
+
+// api key
+fb391254eaf5ba6d50837043f03d0791
