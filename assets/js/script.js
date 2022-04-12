d98762dab88ee869f0d2b05a408bd712c0c2712a
Francella Aguilar <francellaaguilar@Francellas-MacBook-Pro.local>
2022-04-11 21:05:13 -0400

    adding 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ var displayWeather = function(weatherDisplaying) {
 
     // display icon for the weather
     var image = document.createElement('img');
-    image.src = 
+    image.src = `http://openweathermap.org/img/wn/${weatherDisplaying.weather[0].icon}@2x.png`
     currentCity.appendChild(image);
 
     // create span to include temperature data
@@ -79,17 +79,16 @@ var displayWeather = function(weatherDisplaying) {
     weatherDetailContainer.appendChild(windEl);
 
     // display the UV index
-    var lat = weatherResults.coord.lat;
-    var long = weatherResults.coord.lon;
+    var lat = weatherDisplaying.coord.lat;
+    var long = weatherDisplaying.coord.lon;
     uvIndex(lat, lon);
 }
 
 var uvIndex = function(lat, lon) {
     var apiKey = "fb391254eaf5ba6d50837043f03d0791";
     // api url for the uv index function
-    var apiUrl = ``
-
-    //make request to url
+    var apiUrl = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`
+    //make request to api url
     fetch(apiUrl).then(function(response){
         response.json().then(function(data){
             displayUvIndex(data);
@@ -119,3 +118,82 @@ var displayUvIndex = function(index) {
     uvIndexEl.appendChild(uvIndexValue);
     weatherDetailContainer.appendChild(uvIndexEl);
 }
+
+// create variable for 5 day forecast
+
+var fiveDayForecast = function(city) {
+    forecastContainerEl.textContent = "fb391254eaf5ba6d50837043f03d0791";
+    var apiKey = "";
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiUrl).then(response => response.json())
+    .then(data => {
+        console.log(data)
+        displayFiveDayForecast(data);
+    });
+};
+
+var displayFiveDayForecast = function(weatherDisplaying) {
+    var forecast = weatherDisplaying.list;
+    for(var i =5; i < forecast.length; i= i + 8){
+        var everydayForecast = forecast[i];
+
+        var forecastEl = document.createElement("div");
+        forecastEl.classList = "card bg-primary text-light m-2";
+
+        console.log(everydayForecast);
+
+        // display the date for the next 5 days from day entered
+        var forecastDate = document.createElement("h5")
+        forecastDate.textContent = moment.unix(everydayForecast.dt).format("MMM D, YYYY");
+        forecastDate.classList = "card-header text-center"
+        forecastEl.appendChild(forecastDate);
+
+        // show the icon for the weather
+        var forecastIcon = document.createElement('img');
+        forecastIcon.src = `http://openweathermap.org/img/wn/${everydayForecast.weather[0].icon}@2x.png`
+        forecastIcon.style.width = '30%';
+        forecastEl.appendChild(forecastIcon);
+
+        // display the temperature for the 5 days
+        var forecastTemperature = document.createElement("span");
+        forecastTemperature.textContent = "Temperature: " + everydayForecast.main.temp + " °F";
+        forecastTemperature.classList = "card-body";
+        forecastEl.appendChild(forecastTemperature);
+
+        // display the wind for the 5 days
+        var forecastWind = document.createElement("span");
+        forecastWind.textContent = "Wind: " + everydayForecast.wind.speed + " MPH";
+        forecastEl.appendChild(forecastWind);
+
+        // display the humidity for the 5 days 
+        var forecastHumidity = document.createElement("span");
+        forecastHumidity.textContent = "Humidity: " + everydayForecast.main.humidity + " %";
+        forecastHumidity.classList = "card-body";
+        forecastEl.appendChild(forecastHumidity);
+
+        // append all
+        forecastContainerEl.appendChild(forecastEl);
+        }
+}
+
+var searchHistory = function(searchHistory) {
+    console.log(searchH);
+
+    var searchHistoryEl = document.createElement("button");
+    searchHistoryEl.textContent = searchHistory;
+    searchHistoryEl.setAttribute("data-city", searchHistory);
+    searchHistoryEl.setAttribute("type", "submit");
+
+    searchHistoryBtn.prepend(searchHistoryEl);
+}
+
+var previousSearch = function(event) {
+    var city = event.target.getAttribute("data-city");
+    if (city) {
+        getWeather(city);
+        fiveDayForecast(city);
+    }
+}
+cityFormEl.addEventListener("submit", formSubmitHandler);
+searchHistoryBtn.addEventListener("click", previousSearch);
